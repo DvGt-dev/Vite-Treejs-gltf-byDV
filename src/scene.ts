e6799bf1d7909e1c80664ed7ddede3c5f132cad1
djontsovictorien@gmail.com <121665000+DvGt-dev@users.noreply.github.com>
2024-06-18 16:24:15 +0200

    Initial commit

---
+++ src/scene.ts
@@ -0,0 +1,399 @@
+import GUI from 'lil-gui'
+import {
+  AmbientLight,
+  AxesHelper,
+  BoxGeometry,
+  Clock,
+  GridHelper,
+  LoadingManager,
+  Mesh,
+  MeshLambertMaterial,
+  MeshStandardMaterial,
+  PCFSoftShadowMap,
+  PerspectiveCamera,
+  PlaneGeometry,
+  PointLight,
+  PointLightHelper,
+  Scene,
+  WebGLRenderer,
+} from 'three'
+import { DragControls } from 'three/examples/jsm/controls/DragControls'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import * as animations from './helpers/animations'
+import { toggleFullScreen } from './helpers/fullscreen'
+import { resizeRendererToDisplaySize } from './helpers/responsiveness'
+import './style.css'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'; 
+import { Font } from 'three/examples/jsm/fonts/Font.js';
+import * as THREE from 'three';
+
+const CANVAS_ID = 'scene'
+
+let canvas: HTMLElement
+let renderer: WebGLRenderer
+let scene: Scene
+let loadingManager: LoadingManager
+let ambientLight: AmbientLight
+let pointLight: PointLight
+let cube: Mesh
+let camera: PerspectiveCamera
+let cameraControls: OrbitControls
+let dragControls: DragControls
+let axesHelper: AxesHelper
+let pointLightHelper: PointLightHelper
+let clock: Clock
+let stats: Stats
+let gui: GUI
+
+const animation = { enabled: true, play: true }
+
+init()
+animate()
+
+function init() {
+  // ===== 🖼️ CANVAS, RENDERER, & SCENE =====
+  {
+    canvas = document.querySelector(`canvas#${CANVAS_ID}`)!
+    renderer = new WebGLRenderer({ canvas, antialias: true, alpha: true })
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    renderer.shadowMap.enabled = true
+    renderer.shadowMap.type = PCFSoftShadowMap
+    scene = new Scene()
+  }
+
+  // ===== 👨🏻‍💼 LOADING MANAGER =====
+  {
+    loadingManager = new LoadingManager()
+
+    loadingManager.onStart = () => {
+      console.log('loading started')
+    }
+    loadingManager.onProgress = (url, loaded, total) => {
+      console.log('loading in progress:')
+      console.log(`${url} -> ${loaded} / ${total}`)
+    }
+    loadingManager.onLoad = () => {
+      console.log('loaded!')
+    }
+    loadingManager.onError = () => {
+      console.log('❌ error while loading')
+    }
+  }
+
+  // ===== 💡 LIGHTS =====
+  {
+    ambientLight = new AmbientLight('white', 0.4)
+    pointLight = new PointLight('white', 20, 100)
+    pointLight.position.set(-2, 2, 2)
+    pointLight.castShadow = true
+    pointLight.shadow.radius = 4
+    pointLight.shadow.camera.near = 0.5
+    pointLight.shadow.camera.far = 4000
+    pointLight.shadow.mapSize.width = 2048
+    pointLight.shadow.mapSize.height = 2048
+    scene.add(ambientLight)
+    scene.add(pointLight)
+  }
+
+  // ===== 📦 OBJECTS =====
+  {
+    
+    const sideLength = 1
+    const cubeGeometry = new BoxGeometry(sideLength, sideLength, sideLength)
+    const cubeMaterial = new MeshStandardMaterial({
+      color: '#f69f1f',
+      metalness: 0.5,
+      roughness: 0.7,
+    })
+    cube = new Mesh(cubeGeometry, cubeMaterial)
+    cube.castShadow = true
+    cube.position.y = 0.5
+
+    const cube2 = new  Mesh(cubeGeometry, cubeMaterial)
+    cube2.castShadow = true
+    cube2.position.set(2, 0.5, 0)
+
+
+    const planeGeometry = new PlaneGeometry(3, 3)
+    const planeMaterial = new MeshLambertMaterial({
+      color: 'gray',
+      emissive: 'teal',
+      emissiveIntensity: 0.2,
+      side: 2,
+      transparent: true,
+      opacity: 0.4,
+    })
+    const plane = new Mesh(planeGeometry, planeMaterial)
+    plane.rotateX(Math.PI / 2)
+    plane.receiveShadow = true
+
+    
+    const loader = new GLTFLoader();
+    loader.load('src/ma3D.glb', (gltf: GLTF) => { 
+      scene.add(gltf.scene);
+      const ma3D = gltf.scene;
+      ma3D.position.set(2, 0.9, 2)
+      // Fonction d'animation
+      function animate() {
+        requestAnimationFrame(animate);
+        if (animation.enabled && animation.play) {
+          animations.rotate(ma3D, clock, Math.PI / 3);
+          animations.bounce(ma3D, clock, 1, 0.5, 0.5);
+          ma3D.rotation.x += 0.01;
+          ma3D.rotation.y += 0.01;
+        }
+        renderer.render(scene, camera);
+      }
+      animate();
+      
+    });
+   
+    // //eye objet ------------------------------
+    // const loader = new GLTFLoader();
+    // loader.load('scene.gltf', (gltf: GLTF) => { 
+    //   scene.add(gltf.scene);
+    //   const eye = gltf.scene;
+    //   eye.position.set(2, 0.9, 2)
+    //   // Fonction d'animation
+    //   function animate() {
+    //     requestAnimationFrame(animate);
+    //     if (animation.enabled && animation.play) {
+    //       animations.rotate(eye, clock, Math.PI / 3);
+    //       animations.bounce(eye, clock, 1, 0.5, 0.5);
+    //       eye.rotation.x += 0.01;
+    //       eye.rotation.y += 0.01;
+    //     }
+    //     renderer.render(scene, camera);
+    //   }
+    //   animate();
+      
+    // });
+
+
+
+    const fontLoader = new FontLoader()
+    fontLoader.load('https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/fonts/helvetiker_regular.typeface.json', (font: Font) => {
+      const textGeometry = new TextGeometry('DJONTSO', {
+        font,
+        size: 1,
+        height: 0.5,
+        curveSegments: 12,
+        bevelEnabled: true,
+        bevelThickness: 0.2,
+        bevelSize: 0.1,
+        bevelOffset: 0,
+      })
+      const textMesh = new Mesh(textGeometry, new MeshLambertMaterial({ color: 'blue' }))
+      textMesh.position.set(0, 4, 3)
+      textMesh.scale.set(1, 1, 1)
+      scene.add(textMesh)
+      function animate() {
+        requestAnimationFrame(animate);
+        if (animation.enabled && animation.play) {
+          animations.bounce(textMesh, clock, 1, 1.7, 1.5);
+          // animations.rotate(textMesh, clock, Math.PI / 3);
+          animations.bounce(textMesh, clock, 1, 0.9, 0.5);
+          // textMesh.rotation.x += 0.01;
+          // textMesh.rotation.y += 0.01;
+        }
+        renderer.render(scene, camera);
+      }
+      animate();
+    })
+    
+
+    const generateSphere = (radius: number, widthSegments: number, heightSegments: number) => new Float32Array([
+      ...[...Array(heightSegments + 1).keys()].flatMap(y => 
+        [...Array(widthSegments + 1).keys()].flatMap(x => {
+          const theta = (y * Math.PI) / heightSegments;
+          const phi = (x * 2 * Math.PI) / widthSegments;
+          const sinTheta = Math.sin(theta);
+          const cosTheta = Math.cos(theta);
+          const sinPhi = Math.sin(phi);
+          const cosPhi = Math.cos(phi);
+          return [radius * cosPhi * sinTheta, radius * cosTheta, radius * sinPhi * sinTheta];
+        })
+      )
+    ]);
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(generateSphere(1, 32, 32), 3));
+    const sphere = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true }));
+    sphere.position.set(0, 1, 0);
+    scene.add(sphere);
+    const animate2 = () => {
+      requestAnimationFrame(animate2);
+      sphere.rotation.x += 0.01;
+      sphere.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    };
+    animate2();
+
+    
+    scene.add(cube)
+    scene.add(plane)
+    scene.add(cube2)
+  }
+
+  // ===== 🎥 CAMERA =====
+  {
+    camera = new PerspectiveCamera(50, canvas.clientWidth / canvas.clientHeight, 0.1, 100)
+    camera.position.set(2, 2, 5)
+  }
+
+  // ===== 🕹️ CONTROLS =====
+  {
+    cameraControls = new OrbitControls(camera, canvas)
+    cameraControls.target = cube.position.clone()
+    cameraControls.enableDamping = true
+    cameraControls.autoRotate = false
+    cameraControls.update()
+
+    dragControls = new DragControls([cube], camera, renderer.domElement)
+    dragControls.addEventListener('hoveron', (event) => {
+      const mesh = event.object as Mesh
+      const material = mesh.material as MeshStandardMaterial
+      material.emissive.set('orange')
+    })
+    dragControls.addEventListener('hoveroff', (event) => {
+      const mesh = event.object as Mesh
+      const material = mesh.material as MeshStandardMaterial
+      material.emissive.set('black')
+    })
+    dragControls.addEventListener('dragstart', (event) => {
+      const mesh = event.object as Mesh
+      const material = mesh.material as MeshStandardMaterial
+      cameraControls.enabled = false
+      animation.play = false
+      material.emissive.set('black')
+      material.opacity = 0.7
+      material.needsUpdate = true
+    })
+    dragControls.addEventListener('dragend', (event) => {
+      cameraControls.enabled = true
+      animation.play = true
+      const mesh = event.object as Mesh
+      const material = mesh.material as MeshStandardMaterial
+      material.emissive.set('black')
+      material.opacity = 1
+      material.needsUpdate = true
+    })
+    dragControls.enabled = false
+
+    // Full screen
+    window.addEventListener('dblclick', (event) => {
+      if (event.target === canvas) {
+        toggleFullScreen(canvas)
+      }
+    })
+  }
+
+  // ===== 🪄 HELPERS =====
+  {
+    axesHelper = new AxesHelper(4)
+    axesHelper.visible = false
+    scene.add(axesHelper)
+
+    pointLightHelper = new PointLightHelper(pointLight, undefined, 'orange')
+    pointLightHelper.visible = false
+    scene.add(pointLightHelper)
+
+    const gridHelper = new GridHelper(20, 20, 'teal', 'darkgray')
+    gridHelper.position.y = -0.01
+    scene.add(gridHelper)
+  }
+
+  // ===== 📈 STATS & CLOCK =====
+  {
+    clock = new Clock()
+    stats = new Stats()
+    document.body.appendChild(stats.dom)
+  }
+
+  // ==== 🐞 DEBUG GUI ====
+  {
+    gui = new GUI({ title: '🐞 Debug GUI', width: 300 })
+
+    const cubeOneFolder = gui.addFolder('Cube one')
+
+    cubeOneFolder.add(cube.position, 'x').min(-5).max(5).step(0.5).name('pos x')
+    cubeOneFolder.add(cube.position, 'y').min(-5).max(5).step(0.5).name('pos y')
+    cubeOneFolder.add(cube.position, 'z').min(-5).max(5).step(0.5).name('pos z')
+
+    cubeOneFolder.add(cube.material, 'wireframe')
+    cubeOneFolder.addColor(cube.material, 'color')
+    cubeOneFolder.add(cube.material, 'metalness', 0, 1, 0.1)
+    cubeOneFolder.add(cube.material, 'roughness', 0, 1, 0.1)
+
+    cubeOneFolder
+      .add(cube.rotation, 'x', -Math.PI * 2, Math.PI * 2, Math.PI / 4)
+      .name('rotate x')
+    cubeOneFolder
+      .add(cube.rotation, 'y', -Math.PI * 2, Math.PI * 2, Math.PI / 4)
+      .name('rotate y')
+    cubeOneFolder
+      .add(cube.rotation, 'z', -Math.PI * 2, Math.PI * 2, Math.PI / 4)
+      .name('rotate z')
+
+    cubeOneFolder.add(animation, 'enabled').name('animated')
+
+    const controlsFolder = gui.addFolder('Controls')
+    controlsFolder.add(dragControls, 'enabled').name('drag controls')
+
+    const lightsFolder = gui.addFolder('Lights')
+    lightsFolder.add(pointLight, 'visible').name('point light')
+    lightsFolder.add(ambientLight, 'visible').name('ambient light')
+
+    const helpersFolder = gui.addFolder('Helpers')
+    helpersFolder.add(axesHelper, 'visible').name('axes')
+    helpersFolder.add(pointLightHelper, 'visible').name('pointLight')
+
+    const cameraFolder = gui.addFolder('Camera')
+    cameraFolder.add(cameraControls, 'autoRotate')
+
+    // persist GUI state in local storage on changes
+    gui.onFinishChange(() => {
+      const guiState = gui.save()
+      localStorage.setItem('guiState', JSON.stringify(guiState))
+    })
+
+    // load GUI state if available in local storage
+    const guiState = localStorage.getItem('guiState')
+    if (guiState) gui.load(JSON.parse(guiState))
+
+    // reset GUI state button
+    const resetGui = () => {
+      localStorage.removeItem('guiState')
+      gui.reset()
+    }
+    gui.add({ resetGui }, 'resetGui').name('RESET')
+
+    gui.close()
+  }
+}
+
+function animate() {
+  requestAnimationFrame(animate)
+
+  stats.update()
+
+  if (animation.enabled && animation.play) {
+    animations.rotate(cube, clock, Math.PI / 3)
+    animations.bounce(cube, clock, 1, 0.5, 0.5)
+
+    
+  }
+
+  if (resizeRendererToDisplaySize(renderer)) {
+    const canvas = renderer.domElement
+    camera.aspect = canvas.clientWidth / canvas.clientHeight
+    camera.updateProjectionMatrix()
+  }
+
+  cameraControls.update()
+
+  renderer.render(scene, camera)
+}
